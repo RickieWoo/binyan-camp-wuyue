3faeb5992ea0bd04b248e1ebc79f0771d79bde6e
rick <kylin792884522@outlook.com>
2017-07-07 17:29:26 +1400

    删掉了不必要的注释

--- task4/js/index.js
+++ task4/js/index.js
@@ -23,29 +23,21 @@ $(function(){
 });
 
 function addData(result){
-	$.each(result,function(index,obj){
-		//$("#article").append("<div class='artcle-inner'>")+marked(obj['article'])+"</div>");
-		//$(".comment").append() 评论功能先放置
+    $.each(result,function(index,obj){
         var link = obj['alt'];
         var img = obj['images']['medium'];
         var tit = obj['title'];
         var score = obj['rating']['average'];
-	  $(".item-list").append("<li class='flag' role='link' href='"
-    +link+"'> <a class='figure flag-item' href='"
-    +link+"'> <img alt='' src='"
-    +img +"'> </a> " + "<div class='flag-title'> <a href='##' title=''>"
-    +tit + "</a></div> <div class='flag-score'><span>评分</span><strong>"
-    +score + "</strong> </div> "
-    +"</li>"
-    )
-    console.log(obj['images']['medium']);
-    if(index>9)
-    return;
-    // +"'class='article-box'>"+"<article class='article-inner'>"+"<a href="
-    // +obj['article-link']+"><h1 class='article-title'>"
-    // +obj['article-title']+"</h1><p class='article-content-view'>"
-    // +obj['article-inner']+"</p><time class='time-stamp' title="
-    // +obj['article-timestamp']+">"
-    // +obj['article-datetime']+"</time></a></article></div>")
-  })
+        $(".item-list").append("<li class='flag' role='link' href='"
+            +link+"'> <a class='figure flag-item' href='"
+            +link+"'> <img alt='' src='"
+            +img +"'> </a> " + "<div class='flag-title'> <a href='##' title=''>"
+            +tit + "</a></div> <div class='flag-score'><span>评分</span><strong>"
+            +score + "</strong> </div> "
+            +"</li>"
+        )
+        console.log(obj['images']['medium']);
+        if(index>9)
+        return;
+    })
 }
